4713f34b4ec36da100453b9145416600e7a0df31
Joshua Conero <brximl@163.com>
2018-10-30 12:48:15 +0800

    初始化仓库

---
+++ bin/app.go
@@ -0,0 +1,11 @@
+package bin
+
+// @Date：   2018/10/30 0030 12:40
+// @Author:  Joshua Conero
+// @Name:    应用管理
+
+type App struct {
+}
+
+
+
---
+++ bin/router.go
@@ -0,0 +1,11 @@
+package bin
+
+// @Date：   2018/10/30 0030 12:41
+// @Author:  Joshua Conero
+// @Name:    bin 路由器
+
+
+type Router struct {
+	App string					// App 应用名
+}
+
