e0b52b1ff2ada1f53ba3909635590c71e1ee81c2
Conero <conero@163.com>
2020-06-18 17:55:46 +0800

    xsql/builder: add the support ADD

--- xsql/builder_test.go
+++ xsql/builder_test.go
@@ -6,6 +6,7 @@ func TestTable(t *testing.T) {
 	var vSql string
 	var binds []interface{}
 
+	//select
 	var adminUser = Table("admin_user")
 	vSql, binds = adminUser.ToSQL()
 	t.Log(vSql, binds)
@@ -14,4 +15,34 @@ func TestTable(t *testing.T) {
 	vSql, binds = adminUser.ToSQL()
 	t.Log(vSql, binds)
 
+	adminUser.Page(1, 15)
+	vSql, binds = adminUser.ToSQL()
+	t.Log(vSql, binds)
+
+	adminUser.Page(37)
+	vSql, binds = adminUser.ToSQL()
+	t.Log(vSql, binds)
+
+	//for paginate
+	for i := 38; i < 45; i++ {
+		adminUser.Page(i)
+		vSql, binds = adminUser.ToSQL()
+		t.Log(vSql, binds)
+	}
+
+	//update
+	adminUser.Update(map[string]interface{}{"allow_login": true, "test_mk": "test u value", "ability": 33.5}).
+		ResetWhere("id = ?", 1103).Where("gender = ?", 1)
+	vSql, binds = adminUser.ToSQL()
+	t.Log(vSql, binds)
+
+	//delete
+	adminUser.Delete().ResetWhere("id = ?", 1103).Where("gender = ?", 1)
+	vSql, binds = adminUser.ToSQL()
+	t.Log(vSql, binds)
+
+	//insert
+	adminUser.Insert(map[string]interface{}{"allow_login": true, "test_mk": ".test u value.", "ability": 33.5})
+	vSql, binds = adminUser.ToSQL()
+	t.Log(vSql, binds)
 }
