ace6db1114a998e91681fe7e02c1dc676ce8819d
conero <conero@163.com>
2020-07-30 10:36:44 +0800

    fixed: xsql package error likw  'NULL to string is unsupported' in data sql

--- xsql/xsql.go
+++ xsql/xsql.go
@@ -49,31 +49,24 @@ func (c *Query) RowsDick(rows *sql.Rows) ([]map[string]interface{}, error) {
 
 	receive := make([]interface{}, len(columns))
 	for index, _ := range receive {
-		cType := cTypes[index]
+			cType := cTypes[index]
 
-		dataTypeStr := strings.ToUpper(cType.DatabaseTypeName())
-		switch dataTypeStr {
-		case "INT":
-			var a int64
-			receive[index] = &a
-		//@todo fail to map
-		//case "DATETIME", "DATE":
-		//	var a interface{}
-		//	receive[index] = &a
-		case "SMALLINT", "TINYINT":
-			var a int
-			receive[index] = &a
-		case "VARCHAR", "CHAR", "TEXT":
-			var a string
-			receive[index] = &a
-		case "DECIMAL":
-			var a float64
-			receive[index] = &a
-		default:
-			var a interface{}
-			receive[index] = &a
+			dataTypeStr := strings.ToUpper(cType.DatabaseTypeName())
+			switch dataTypeStr {
+			case "INT", "SMALLINT", "TINYINT":
+				var a sql.NullInt64
+				receive[index] = &a
+			case "VARCHAR", "CHAR", "TEXT":
+				var a sql.NullString
+				receive[index] = &a
+			case "DECIMAL":
+				var a sql.NullFloat64
+				receive[index] = &a
+			default:
+				var a interface{}
+				receive[index] = &a
+			}
 		}
-	}
 
 	var dataList []map[string]interface{}
 	var counter = 0
@@ -90,24 +83,37 @@ func (c *Query) RowsDick(rows *sql.Rows) ([]map[string]interface{}, error) {
 
 			dataTypeStr := strings.ToUpper(cType.DatabaseTypeName())
 			switch dataTypeStr {
-			case "INT":
-				item[col] = *v.(*int64)
-			case "DATETIME", "DATE":
-				tmpValue := *v.(*interface{})
-				var timeStr string
-				if tmpValue != nil {
-					timeStr = fmt.Sprintf("%s", tmpValue)
+				case "INT","SMALLINT", "TINYINT":
+					var anyVal =  *v.(*sql.NullInt64)
+					if anyVal.Valid{
+						item[col] = anyVal.Int64
+					}else{
+						item[col] = nil
+					}
+				case "DATETIME", "DATE":
+					tmpValue := *v.(*interface{})
+					var timeStr string
+					if tmpValue != nil {
+						timeStr = fmt.Sprintf("%s", tmpValue)
+					}
+					item[col] = timeStr
+				case "VARCHAR", "CHAR", "TEXT":
+					var anyVal =  *v.(*sql.NullString)
+					if anyVal.Valid{
+						item[col] = anyVal.String
+					}else{
+						item[col] = ""
+					}
+				case "DECIMAL":
+					var anyVal =  *v.(*sql.NullFloat64)
+					if anyVal.Valid{
+						item[col] = anyVal.Float64
+					}else{
+						item[col] = nil
+					}
+				default:
+					item[col] = *v.(*interface{})
 				}
-				item[col] = timeStr
-			case "SMALLINT", "TINYINT":
-				item[col] = *v.(*int)
-			case "VARCHAR", "CHAR", "TEXT":
-				item[col] = *v.(*string)
-			case "DECIMAL":
-				item[col] = *v.(*float64)
-			default:
-				item[col] = *v.(*interface{})
-			}
 
 		}
 
