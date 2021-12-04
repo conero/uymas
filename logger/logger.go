784759c32d101e9ff2e1ebbcfecd3f3450dba302
Joshua Conero <conero@163.com>
2021-12-04 14:26:12 +0800

    feat(logger): add new package logger for appliction logging more than go emebd log

---
+++ logger/logger.go
@@ -0,0 +1,109 @@
+// Package logger basic and simple logger for application
+package logger
+
+import (
+	"fmt"
+	"gitee.com/conero/uymas/bin/butil"
+	"gitee.com/conero/uymas/fs"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+type Level int8
+
+const (
+	LogDebug Level = iota
+	LogInfo
+	LogWarning
+	LogError
+	LogNone
+)
+
+type Logger struct {
+	logger *log.Logger
+	Level  Level
+}
+
+// Config 日志配置文件
+type Config struct {
+	Log       *log.Logger
+	Level     string
+	Driver    string // 输出启驱动<stdout, file>
+	OutputDir string // 输出日志，设置是为文件驱动
+}
+
+func (l Logger) Format(prefix, message string, args ...interface{}) {
+	l.logger.Printf("[%v] %v", prefix, fmt.Sprintf(message, args...))
+}
+
+func (l Logger) Debugf(message string, args ...interface{}) {
+	if l.Level > LogDebug {
+		return
+	}
+	l.Format("DEBUG", message, args...)
+}
+
+func (l Logger) Infof(message string, args ...interface{}) {
+	if l.Level > LogInfo {
+		return
+	}
+	l.Format("INFO", message, args...)
+}
+
+func (l Logger) Warningf(message string, args ...interface{}) {
+	if l.Level > LogWarning {
+		return
+	}
+	l.Format("WARNING", message, args...)
+}
+
+func (l Logger) Errorf(message string, args ...interface{}) {
+	if l.Level > LogError {
+		return
+	}
+	l.Format("ERR", message, args...)
+}
+
+func NewLogger(cfg Config) *Logger {
+	lv := LogDebug
+	switch strings.ToLower(cfg.Level) {
+	case "error", "err":
+		lv = LogError
+	case "warning", "warn":
+		lv = LogWarning
+	case "info":
+		lv = LogInfo
+	case "debug":
+		lv = LogDebug
+	case "none", "no", "mute", "quiet":
+		lv = LogNone
+	}
+
+	if cfg.Log == nil { // 默认日志
+		if lv != LogNone && (cfg.Driver == "file" || cfg.OutputDir != "") {
+			output := cfg.OutputDir
+			if output == "" {
+				output = butil.GetPathDir("/.runtime")
+			}
+			now := time.Now()
+			output = fs.CheckDir(fmt.Sprintf("%v/%v", output, now.Format("2006/01")))
+			fl, er := os.OpenFile(fmt.Sprintf("%v/%v.log", output, now.Format("02")),
+				os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0777)
+			if er == nil {
+				cfg.Log = log.New(fl, "\n", log.LstdFlags)
+			}
+		}
+
+		// 降级处理，所有驱动解析失败的使用控制台
+		if cfg.Log == nil {
+			cfg.Log = log.New(os.Stdout, "\n", log.LstdFlags)
+		}
+	}
+
+	return &Logger{
+		logger: cfg.Log,
+		Level:  lv,
+	}
+}
