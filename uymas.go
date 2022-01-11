d2a24f75af4f8a62662d709fd2db35c5a19f7b28
Joshua Conero <conero@163.com>
2022-01-11 09:14:26 +0800

    doc: optimize project documents and ready to mark new tag v1.1.0

--- uymas.go
+++ uymas.go
@@ -1,13 +1,17 @@
+// Package uymas is summary util library from the work experience,
+// provides base version, author information and so on.
+//
+// Major functional like `bin` (CLI-APP), `str`(string util), other more. Some of originated from work experience
+// to enhance code reuse.
+// The final directory is a convenient tool to realize command-line program development and other code.
+package uymas
+
 // @Date：   2018/10/30 0030 12:58
 // @Author:  Joshua Conero
 
-// Package uymas is summary util library from the work experience,
-//provides base version, author information and so on.
-package uymas
-
 const (
-	Version        = "1.1.0-ea4" //dev is not product but development tag.
-	Release        = "dev"
+	Version        = "1.1.0" //dev is not product but development tag.
+	Release        = "2022-01-11"
 	Since          = "20181030"
 	Author         = "Joshua Conero"
 	Email          = "conero@163.com"
