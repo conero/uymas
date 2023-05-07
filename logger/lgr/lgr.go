65d3cc51423a61bfd1588b78b7412c7e7ec37566
Joshua Conero <conero@163.com>
2023-05-07 19:53:56 +0800

    feat(logger/lgr): add package lgr as simple instance of logger for cli etc.

---
+++ logger/lgr/lgr.go
@@ -0,0 +1,30 @@
+// Package lgr An instance of library `logger`, used for direct output from the command line, etc
+package lgr
+
+import "gitee.com/conero/uymas/logger"
+
+var vLgr *logger.Logger
+
+func init() {
+	vLgr = logger.NewLogger()
+}
+
+func Log() logger.Logger {
+	return *vLgr
+}
+
+func Debug(message string, args ...any) {
+	vLgr.Debugf(message, args...)
+}
+
+func Info(message string, args ...any) {
+	vLgr.Infof(message, args...)
+}
+
+func Warn(message string, args ...any) {
+	vLgr.Warnf(message, args...)
+}
+
+func Error(message string, args ...any) {
+	vLgr.Errorf(message, args...)
+}
