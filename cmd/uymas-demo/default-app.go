4914a682a7fd5624706499e9023448e9c6907e91
Joshua Conero <conero@163.com>
2023-09-05 11:59:44 +0800

    pref(str): Calc Numerical operation implementation

--- cmd/uymas-demo/default-app.go
+++ cmd/uymas-demo/default-app.go
@@ -6,8 +6,6 @@ import (
 	"gitee.com/conero/uymas/logger/lgr"
 	"gitee.com/conero/uymas/str"
 	"regexp"
-	"strconv"
-	"strings"
 )
 
 type defaultApp struct {
@@ -33,118 +31,12 @@ func (c *defaultApp) Cal() {
 		return
 	}
 
-	// 计算器（加减乘除）
-	// 字符串转数字
-	str2F64 := func(s string) float64 {
-		f64, _ := strconv.ParseFloat(s, 10)
-		return f64
+	calc := str.NewCalc(equal)
+	calc.Count()
+
+	if c.Cc.CheckSetting("V", "verbose") {
+		lgr.Info("输入等式：%s\n    => %v", c.Cc.SubCommand, calc)
+		return
 	}
-
-	//计算
-	regBracketSign := regexp.MustCompile(`^\(.*\)$`)
-	// add, subtract, multiply and divide => +-*/
-	regMd := regexp.MustCompile(`(\d+(\.\d+)?)[*/](\d+(\.\d+)?)`)
-	mdSig := regexp.MustCompile(`[*/]`)
-	regAs := regexp.MustCompile(`(\d+(\.\d+)?)[+-](\d+(\.\d+)?)`)
-	asSig := regexp.MustCompile(`[+\-]`)
-	asmdSig := regexp.MustCompile(`[+\-*/]+`)
-	calFn := func(bkt string) string {
-		bkt = strings.TrimSpace(bkt)
-		// 去括号
-		if regBracketSign.MatchString(bkt) && len(bkt) > 2 {
-			bkt = bkt[1:]
-			bkt = bkt[:len(bkt)-1]
-		}
-
-		// 乘除运算
-		mdLs := regMd.FindAllString(bkt, -1)
-		for _, md := range mdLs {
-			mdSigLs := mdSig.FindAllString(md, -1)
-			if len(mdSigLs) != 1 {
-				bkt = strings.ReplaceAll(bkt, md, "0")
-				continue
-			}
-			parts := mdSig.Split(bkt, -1)
-			if len(parts) != 2 {
-				bkt = strings.ReplaceAll(bkt, md, "0")
-				continue
-			}
-
-			var mgCtt float64 = 0
-			if mdSigLs[0] == "*" {
-				mgCtt = str2F64(parts[0]) * str2F64(parts[1])
-			} else {
-				mgCtt = str2F64(parts[0]) / str2F64(parts[1])
-			}
-
-			bkt = strings.ReplaceAll(bkt, md, str.FloatSimple(fmt.Sprintf("%.5f", mgCtt)))
-
-		}
-
-		// 加减运算
-		asLs := regAs.FindAllString(bkt, -1)
-		for _, as := range asLs {
-			asSigLs := asSig.FindAllString(as, -1)
-			if len(asSigLs) != 1 {
-				bkt = strings.ReplaceAll(bkt, as, "0")
-				continue
-			}
-			parts := asSig.Split(bkt, -1)
-			if len(parts) != 2 {
-				bkt = strings.ReplaceAll(bkt, as, "0")
-				continue
-			}
-
-			var mgCtt float64 = 0
-			if asSigLs[0] == "+" {
-				mgCtt = str2F64(parts[0]) + str2F64(parts[1])
-			} else {
-				mgCtt = str2F64(parts[0]) - str2F64(parts[1])
-			}
-
-			mgCttStr := fmt.Sprintf("%.5f", mgCtt)
-			bkt = strings.ReplaceAll(bkt, as, str.FloatSimple(mgCttStr))
-		}
-
-		return bkt
-	}
-
-	// 循环
-	regBracket := regexp.MustCompile(`\([^()]+\)`)
-	regEqual := regexp.MustCompile(``)
-	countBad := 0
-	for {
-		isEnd := false
-
-		bracket := regBracket.FindAllString(equal, -1)
-		for _, bkt := range bracket {
-			newBkt := calFn(bkt)
-			equal = strings.ReplaceAll(equal, bkt, newBkt)
-		}
-
-		equal = calFn(equal)
-
-		if !asmdSig.MatchString(equal) {
-			break
-		}
-
-		if equal == "" {
-			break
-		}
-
-		if isEnd {
-			break
-		}
-
-		if !regEqual.MatchString(equal) {
-			break
-		}
-
-		// 大于5000直接退出
-		if countBad > 5000 {
-			break
-		}
-		countBad += 1
-	}
-	lgr.Info("输入等式：%s\n    => %s", c.Cc.SubCommand, equal)
+	fmt.Println(calc)
 }
--- str/calc.go
+++ str/calc.go
@@ -1,28 +1,152 @@
 package str
 
 import (
+	"fmt"
 	"gitee.com/conero/uymas/util/rock"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
 // Calc String equality operator, which calculates the result of input string equality
 // @todo should be completed in v1.3.x
 type Calc struct {
-	equality string
-	result   float64
+	equality     string
+	handlerEq    string
+	result       float64
+	regBracket   *regexp.Regexp // `()`
+	regBracketSg *regexp.Regexp // `()`
+	clearReg     *regexp.Regexp // `space clear`
+	mulDivReg    *regexp.Regexp // `*/`
+	mulDivRegSg  *regexp.Regexp // `*/`
+	addSubReg    *regexp.Regexp
+	addSubRegSg  *regexp.Regexp
+	Accuracy     int8
+	accuracyStr  string
 }
 
 func NewCalc(equality string) *Calc {
 	return &Calc{
 		equality: equality,
+		Accuracy: 7,
 	}
 }
 
+// Bracket decomposition, clear `()`
+func (c *Calc) deBracket() {
+	if c.regBracket == nil {
+		c.regBracket = regexp.MustCompile(`\([^()]+\)`)
+	}
+
+	bracket := c.regBracket.FindAllString(c.handlerEq, -1)
+	for _, brk := range bracket {
+		rslt := c.operNonBrk(brk)
+		c.handlerEq = strings.ReplaceAll(c.handlerEq, brk, rslt)
+	}
+
+	if c.regBracket.MatchString(c.handlerEq) {
+		c.deBracket()
+	}
+}
+
+// Operation without parentheses
+func (c *Calc) operNonBrk(eq string) string {
+	if c.regBracketSg == nil {
+		c.regBracketSg = regexp.MustCompile(`^\(.*\)$`)
+	}
+
+	// clear bracket
+	if c.regBracketSg.MatchString(eq) {
+		eq = eq[1:]
+		eq = eq[:len(eq)-1]
+	}
+
+	// add, subtract, multiply and divide => +-*/
+	eq = c.mulDiv(eq)
+	eq = c.addSub(eq)
+
+	return eq
+}
+
+// multiply and divide
+func (c *Calc) mulDiv(eq string) string {
+	if c.mulDivReg == nil {
+		c.mulDivReg = regexp.MustCompile(`(\d+(\.\d+)?)[*/](\d+(\.\d+)?)`)
+	}
+	if c.mulDivRegSg == nil {
+		c.mulDivRegSg = regexp.MustCompile(`[*/]`)
+	}
+
+	mulDiv := c.mulDivReg.FindAllString(eq, -1)
+	for _, md := range mulDiv {
+		splitLs := c.mulDivRegSg.Split(md, -1)
+		beg := StringAsFloat(splitLs[0])
+		end := StringAsFloat(splitLs[1])
+
+		sgList := c.mulDivRegSg.FindAllString(md, -1)
+		var cul float64
+		if sgList[0] == "*" {
+			cul = beg * end
+		} else {
+			cul = beg / end
+		}
+
+		eq = strings.ReplaceAll(eq, md, FloatSimple(fmt.Sprintf(c.accuracyStr, cul)))
+	}
+
+	return eq
+}
+
+// add, subtract
+func (c *Calc) addSub(eq string) string {
+	if c.addSubReg == nil {
+		c.addSubReg = regexp.MustCompile(`(\d+(\.\d+)?)[+\-](\d+(\.\d+)?)`)
+	}
+	if c.addSubRegSg == nil {
+		c.addSubRegSg = regexp.MustCompile(`[+\-]`)
+	}
+
+	addSub := c.addSubReg.FindAllString(eq, -1)
+	for _, as := range addSub {
+		splitLs := c.addSubRegSg.Split(as, -1)
+		beg := StringAsFloat(splitLs[0])
+		end := StringAsFloat(splitLs[1])
+
+		sgList := c.addSubRegSg.FindAllString(as, -1)
+		var cul float64
+		if sgList[0] == "+" {
+			cul = beg + end
+		} else {
+			cul = beg - end
+		}
+
+		eq = strings.ReplaceAll(eq, as, FloatSimple(fmt.Sprintf(c.accuracyStr, cul)))
+	}
+	return eq
+}
+
 func (c *Calc) Count(args ...string) float64 {
+	c.accuracyStr = "%." + fmt.Sprintf("%d", c.Accuracy) + "f"
 	equality := rock.ExtractParam(c.equality, args...)
+	eq := strings.TrimSpace(equality)
+
+	// Clear interfering characters
+	if c.clearReg == nil {
+		c.clearReg = regexp.MustCompile(`\s+`)
+	}
+	eq = c.clearReg.ReplaceAllString(eq, "")
+
+	c.handlerEq = eq
 	c.equality = equality
+
+	c.deBracket()
+	c.handlerEq = c.operNonBrk(c.handlerEq)
+	c.result = StringAsFloat(c.handlerEq)
 	return c.result
+}
+
+func (c *Calc) String() string {
+	return FloatSimple(fmt.Sprintf(c.accuracyStr, c.result))
 }
 
 // FloatSimple Floating-point number string beautification
@@ -49,4 +173,9 @@ func FloatSimple(fv string) string {
 	}
 
 	return fv
+}
+
+func StringAsFloat(s string) float64 {
+	f64, _ := strconv.ParseFloat(s, 10)
+	return f64
 }
