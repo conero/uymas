0dd4c035e2dc82640a00c3f8bac66692b47a0f5b
Joshua Conero <conero@163.com>
2024-03-01 11:49:37 +0800

    doc: optimize the doc of code

--- util/param.go
+++ util/param.go
@@ -6,7 +6,7 @@ import (
 )
 
 // ExtractParam Extract indefinite parameters from functions and default code values
-// Deprecated: please replace by `rock.ExtractParam`，will remove 1.4
+// Deprecated: please replace by `rock.ExtractParam`，will remove in future
 func ExtractParam[T constraints.Equable](defValue T, args ...T) T {
 	return rock.ExtractParam(defValue, args...)
 }
