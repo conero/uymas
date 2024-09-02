5fc55431f3c1a315bf36bdde26de00650db35523
Joshua Conero <conero@163.com>
2024-09-02 08:53:49 +0800

    doc: plan to next

--- uymas.go
+++ uymas.go
@@ -10,8 +10,8 @@ package uymas
 // @Author:  Joshua Conero
 
 const (
-	Version        = "2.0.0-alpha.3" //dev is not product but development tag.
-	Release        = "20240901"      // dev|20060102
+	Version        = "2.0.0-rc.1" //dev is not product but development tag.
+	Release        = "dev"        // dev|20060102
 	Since          = "20181030"
 	Author         = "Joshua Conero"
 	Email          = "conero@163.com"
