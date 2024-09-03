f3ff8ac8d01b640413f9a477f5e63b319f2c0c2a
Joshua Conero <conero@163.com>
2024-09-03 23:04:25 +0800

    pref(rock): add test case for ListEq, ListSubset

--- rock/set_test.go
+++ rock/set_test.go
@@ -34,3 +34,43 @@ func TestMapKeys(t *testing.T) {
 		t.Errorf("Get map keys failue, like: %#v", keys)
 	}
 }
+
+func TestListEq(t *testing.T) {
+	intArr1 := []int{1, 9, 49, 1001, 24, 903}
+	intArr2 := []int{903, 24, 49}
+
+	// case
+	if ListEq(intArr1, intArr2) {
+		t.Errorf("%#v = %#v，次判别错误", intArr1, intArr2)
+	}
+
+	// case
+	intArr2 = []int{903, 24, 1001, 49, 9, 1}
+	if !ListEq(intArr1, intArr2) {
+		t.Errorf("%#v ≠ %#v，次判别错误", intArr1, intArr2)
+	}
+
+	intArr1, intArr2 = nil, nil
+	if !ListEq(intArr1, intArr2) {
+		t.Errorf("%#v ≠ %#v，次判别错误", intArr1, intArr2)
+	}
+
+}
+
+func TestListSubset(t *testing.T) {
+	intArr1 := []int{1, 9, 49, 1001, 24, 903}
+	intArr2 := []int{903, 24, 49}
+
+	// case
+	if !ListSubset(intArr1, intArr2) {
+		t.Errorf("%#v 应该为 %#v 的子数组，次判别错误", intArr1, intArr2)
+	}
+
+	strArr1 := `I am Jc, Coder.`
+	strArr2 := "Coder."
+
+	// case
+	if !ListSubset([]byte(strArr1), []byte(strArr2)) {
+		t.Errorf("%#v 应该为 %#v 的子数组，次判别错误", intArr1, intArr2)
+	}
+}
