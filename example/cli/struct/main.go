82b4af4af875a0adaed551fb1831a12752620462
Joshua Conero <conero@163.com>
2024-09-28 21:17:22 +0800

    pref(example/cli/struct): add cust command for user-defined

--- example/cli/struct/main.go
+++ example/cli/struct/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"fmt"
+	"gitee.com/conero/uymas/v2/cli"
 	"gitee.com/conero/uymas/v2/cli/gen"
 	"gitee.com/conero/uymas/v2/logger/lgr"
 )
 
 func main() {
 	app := gen.AsCommand(new(defaultApp))
+	// User-defined external registration commands
+	app.Command(func(parser cli.ArgsParser) {
+		fmt.Println("用户自定义命令，JC")
+	}, "cust", cli.Help("用户自定义命令"))
 	lgr.ErrorIf(app.Run())
 }
