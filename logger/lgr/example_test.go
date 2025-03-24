510dac2dda4ee5de8da077be0293da2e0fa5acd0
Joshua Conero <conero@163.com>
2025-03-24 11:28:43 +0800

    doc: optimize doc or test case

--- logger/lgr/example_test.go
+++ logger/lgr/example_test.go
@@ -1,10 +1,14 @@
 package lgr
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func ExampleDebug() {
 	Debug("This is Debug logger.")
 	TmpMark("temp mark when develop.")
+	TmpMark("param: %s, Year %d", "Joshua Conero", time.Now().Year())
 }
 
 func ExampleError() {
