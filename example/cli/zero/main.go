edc39b7e8c9cf73765375a0d245ccb362330cd40
Joshua Conero <conero@163.com>
2025-10-26 23:25:04 +0800

    doc(example/cli/zero): add build help comment for

--- example/cli/zero/main.go
+++ example/cli/zero/main.go
@@ -4,6 +4,10 @@ import (
 	"gitee.com/conero/uymas/v2/cli"
 )
 
+// zero 代码测试
+//
+// 压缩模式执行（删除调试信息/可用于生产）： go build -ldflags "-s -w" ./example/cli/zero/
+// 普通打包： go build ./example/cli/zero/
 func main() {
 	app := cli.NewCli()
 	_ = app.Run()
